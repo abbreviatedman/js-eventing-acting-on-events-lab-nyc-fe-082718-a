a8e64d498a2869bfb522c11e00b3598c5bf08af6
Colin Jaffe <colin.jaffe@gmail.com>
2018-10-17 22:17:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+
+
+// const dodger = document.querySelector('#dodger')
+
+// const getLeft = () => dodger.style.left.replace('px', '')
+
+// const getNewPosition = (leftSide, xChange) => parseInt(leftSide, 10) + xChange
+
+
+// dodger.style.backgroundColor = 'red'
+
+// document.addEventListener('keydown', handleKey)
+
+// function handleKey(event) {
+//   const key = event.which
+//   switch(key) {
+//     case 37:
+//       moveDodgerLeft()
+//       break;
+//     case 39:
+//       moveDodgerRight()
+//       break;
+//   }
+// }
+
+// function moveDodgerLeft() {
+//   const left = getLeft()
+//   const newPosition = getNewPosition(left, -1)
+  
+//   if (left >= 0) {
+//     dodger.style.left = newPosition + 'px'
+//   }
+// }
+
+// function moveDodgerRight() {
+//   const left = getLeft()
+//   const newPosition = getNewPosition(left, +1)
+  
+//   if (left <= 360) {
+//     dodger.style.left = newPosition + 'px'
+//   }
+// }
+
+
