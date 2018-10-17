85f8fd95d8aa5c0b4aebb6405a5f22d4207da5c7
Colin Jaffe <colin.jaffe@gmail.com>
2018-10-17 23:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
-  if (left < 220) {
+  if (left < 400) {
     dodger.style.left = `${left + 1}px`
   }
 }
