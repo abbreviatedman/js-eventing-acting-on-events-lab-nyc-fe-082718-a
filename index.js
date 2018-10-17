cad5bf6f6e3b536ac2411763c4aa727693e79047
Colin Jaffe <colin.jaffe@gmail.com>
2018-10-17 23:08:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-const dodger = document.querySelector('#dodger');
+// const dodger = document.querySelector('#dodger');
 
-dodger.style.backgroundColor = 'pink'
+// dodger.style.backgroundColor = 'pink'
 
-document.addEventListener('keydown', function(event) {
-  if (event.which === 37) {
-    moveDodgerLeft()
-  } else if (event.which === 39) {
-    moveDodgerRight()
-  }
-})
-
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-  if (left > 0) {
-    dodger.style.left = `${left - 5}px`
-  }
-}
-
-function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-  if (left <= 360) {
-    dodger.style.left = `${left + 5}px`
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const dodger = document.querySelector('#dodger')
-
-// const getLeft = () => dodger.style.left.replace('px', '')
-
-// const getNewPosition = (leftSide, xChange) => parseInt(leftSide, 10) + xChange
-
-
-// dodger.style.backgroundColor = 'red'
-
-// document.addEventListener('keydown', handleKey)
-
-// function handleKey(event) {
-//   const key = event.which
-//   switch(key) {
-//     case 37:
-//       moveDodgerLeft()
-//       break;
-//     case 39:
-//       moveDodgerRight()
-//       break;
+// document.addEventListener('keydown', function(event) {
+//   if (event.which === 37) {
+//     moveDodgerLeft()
+//   } else if (event.which === 39) {
+//     moveDodgerRight()
 //   }
-// }
+// })
 
 // function moveDodgerLeft() {
-//   const left = getLeft()
-//   const newPosition = getNewPosition(left, -1)
-  
-//   if (left >= 0) {
-//     dodger.style.left = newPosition + 'px'
+//   var leftNumbers = dodger.style.left.replace('px', '')
+//   var left = parseInt(leftNumbers, 10)
+//   if (left > 0) {
+//     dodger.style.left = `${left - 5}px`
 //   }
 // }
 
 // function moveDodgerRight() {
-//   const left = getLeft()
-//   const newPosition = getNewPosition(left, +1)
-  
+//   var leftNumbers = dodger.style.left.replace('px', '')
+//   var left = parseInt(leftNumbers, 10)
 //   if (left <= 360) {
-//     dodger.style.left = newPosition + 'px'
+//     dodger.style.left = `${left + 5}px`
 //   }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const dodger = document.querySelector('#dodger')
+
+const getLeft = () => dodger.style.left.replace('px', '')
+
+const getNewPosition = (leftSide, xChange) => parseInt(leftSide, 10) + xChange
+
+
+dodger.style.backgroundColor = 'red'
+
+document.addEventListener('keydown', handleKey)
+
+function handleKey(event) {
+  const key = event.which
+  switch(key) {
+    case 37:
+      moveDodgerLeft()
+      break;
+    case 39:
+      moveDodgerRight()
+      break;
+  }
+}
+
+function moveDodgerLeft() {
+  const left = getLeft()
+  const newPosition = getNewPosition(left, -1)
+  
+  if (left >= 0) {
+    dodger.style.left = newPosition + 'px'
+  }
+}
+
+function moveDodgerRight() {
+  const left = getLeft()
+  const newPosition = getNewPosition(left, +1)
+  
+  if (left <= 360) {
+    dodger.style.left = newPosition + 'px'
+  }
+}
 
 
