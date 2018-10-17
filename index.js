e04a028577456c0751b4a9cbc34411302e29fa87
Colin Jaffe <colin.jaffe@gmail.com>
2018-10-17 22:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const dodger = document.querySelector('#dodger');
 
 dodger.style.backgroundColor = 'pink'
 
-
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
 
 
 
